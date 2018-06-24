f771e2b3f08d1cd2d4131ba4f2e91dcc4a3f6a46
blaz.sasek <blaz.sasek@gmail.com>
2018-06-24 18:02:52 +0200

    static fix

--- tozd/urls.py
+++ tozd/urls.py
@@ -39,6 +39,5 @@ urlpatterns = [
     path('reset/done/', password_reset_complete, name='password_reset_complete'),
 ]
 
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
